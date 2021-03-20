660c6f0f7bffafee8f56f18e57d4a7e204c58743
SimulacraAtTanagra <shaneayers1@gmail.com>
2021-03-20 09:42:24 -0400

    updated PAF_base.py

--- PAF_base.py
+++ PAF_base.py
@@ -7,6 +7,7 @@ Created on Fri Mar 19 13:22:29 2021
 
 from admin import read_json, select_thing
 from pdfops import write_fillable_pdf
+import os
 
 fields=['Departments', 'Program', #cjr
  'First Name', 'Last Name', 'Initial_4', 'EmplID', 'No', 'Street', 'Apt', #cjr
@@ -16,7 +17,7 @@ fields=['Departments', 'Program', #cjr
  'Action', 'Reason',    #from user input + hr guide file
  'Effective Date', 'Or Effective Period Starting', 'and Ending', #from user input
  'Salary amount', 'Salary', 'Per Annum', 'Fraction of Annual Rate', #from user input
- 'Remarks'] #from user input + HR Guide actions
+ 'Remarks','idfield'] #from user input + HR Guide actions, from os count
 #Phase 1 - determine what type of operation 
 #single, multi-person, multi-action, or multi-person,multi-action (pattern)
 template=r'Y:\Templates\Autotemplates\Personnel Action Form.pdf'
@@ -32,17 +33,29 @@ def phase1():
 #Phase 2 - identify person or persons based on several criteria
 #fuzzy name lookup, emplid, category of staff, department, etc
 def matching_criteria(criteria):
-    if criteria in critdict:#some dictionary of all possible items and their classes
-        category=critdict[criteria]
-        
+    if criteria in critdict:    #simple json lookup
+        category=critdict[criteria] #returns appropriate json location
+    return(read_json(category))
+def efficient_search(selection,dictionary):
+    
+    searchstr=input(
+            f"Please type a portion of the {selection} for this search:\n"
+            )
+    newdict={k:dictionary[k] for k in dictionary.keys() if searchstr in k}
+    print("These are your results:")
+    selection=select_thing([k for k in newdict.keys()])
+    return(newdict[selection])
+    
 def select_person():
     #this is where we look for at least one person
     #perhaps using some sort of lookup?
     #idk figure it out, shane
-    options=['Name','Emplid','Title Group','Department'] #need data source here
+    options=['Name','Emplid','Category of Staff','Title','Department'] #need data source here
     selection=select_thing(options)
-    
+    searchdict=matching_criteria(selection)
+        
     return(persondict)
+
 def phase2(inst_str:str):   #accepts instruction string from phase1
     
     
